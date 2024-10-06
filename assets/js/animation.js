974604c8d38f8bcef9f25af33a89051c05213488
Danih RAKOTOARISON <danih.rakotoarison@gmail.com>
2024-10-06 21:02:06 +0300

    Integration de l'animation

---
+++ assets/js/animation.js
@@ -0,0 +1,17 @@
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    console.log('scroll');
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let triggerOffset = 300;  // Adjust this value to trigger the animation earlier
+
+        if (top >= offset - triggerOffset && top < offset + height){
+            sec.classList.add('show-animate');
+        }else{
+            sec.classList.remove('show-animate');
+        }
+    })
+}
